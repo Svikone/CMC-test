6b16c4d2a999bfc9f84a17d685f81985c6cfd4f6
svikone <velichkovlad825@gmail.com>
2020-05-29 23:51:25 +0300

    fix deployment

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
-
+app.get("/",(req, res) => {
+    res.sendFile(__dirname+"/index.html")
+})
 app.listen(port,() => {
     console.log("server started 9000")
 })
