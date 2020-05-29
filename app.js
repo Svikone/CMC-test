6b672d80e0298a0ce3aa8fe134cd40fef6b7a0d6
svikone <velichkovlad825@gmail.com>
2020-05-30 00:01:22 +0300

    fix static file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const serveStatic = require("serve-static");
 const app = express();
+app.use(serveStatic(__dirname)) 
 const port = process.env.PORT || 9000;
 app.get("/",(req, res) => {
     res.sendFile(__dirname+"/index.html")
