2bcfb5af461ec90c01161daa71181c5d56ed926a
svikone <velichkovlad825@gmail.com>
2020-05-29 23:14:14 +0300

    add modal to item

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-    })
-    $('.btn').click(()=> {
+    $('a.scollTo').on('click', function(e){
         $('.modal').modal('hide')
+        var $anchor = $(this).attr("data-href");
+        var $hrefStart = $anchor.substr(0, 1);
 
+        if ( $hrefStart == "#" ) {
+           $('html, body').animate({
+               scrollTop: $($anchor).offset().top
+           }, 1500, 'easeInOutExpo');
+
+           e.preventDefault(); 
+
+        } else {
+           window.location.href = $anchor;
+        }
     })
 })
